6f9e23dbccc3c93305ecff5e782b7b67bd078390
CameronOsipenko7874 <77690085+CameronOsipenko7874@users.noreply.github.com>
2021-06-02 20:11:04 -0400

    Commit #1: Adding author and date tag.

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -1,21 +1,23 @@
 package pickacard;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in Exercise 1. When you create your own branch,
- * add your name as an author.
+ * A class that models playing card Objects. Cards have a value (note that Ace =
+ * 1, Jack =11, Queen =12, King = 13) A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers. This code is to be used in Exercise
+ * 1. When you create your own branch, add your name as an author.
+ * 
  * @author dancye
  * @author Paul Bonenfant
+ * @author Cameron Osipenko
+ * @date June 2, 2021
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    private String suit; // clubs, spades, diamonds, hearts
+    private int value;// 1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    public static final String[] SUITS = { "Hearts", "Diamonds", "Spades", "Clubs" };
+
     /**
      * @return the suit
      */
@@ -29,7 +31,5 @@ public class Card {
     public void setSuit(String suit) {
         this.suit = suit;
     }
-   
-   
-    
+
 }
