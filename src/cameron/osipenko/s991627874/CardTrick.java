543b8250ff8d74b6f4dca19494919a9c44d19db5
CameronOsipenko7874 <77690085+CameronOsipenko7874@users.noreply.github.com>
2021-06-05 18:11:46 -0400

    Commit #14: Actual final commit. Fixed Java Docs

--- src/cameron/osipenko/s991627874/CardTrick.java
+++ src/cameron/osipenko/s991627874/CardTrick.java
@@ -2,8 +2,7 @@ package cameron.osipenko.s991627874;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then
- * asks the user to pick a card and searches the array of cards for the match to
- * the user's card. To be used as starting code in ICE 1
+ * checks those cards against pre-set lucky card.
  *
  * @author dancye
  * @author Paul Bonenfant
